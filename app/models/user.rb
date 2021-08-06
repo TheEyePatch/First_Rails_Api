4fd354a5164fbcc7e398895d847106e78dc70f14
Leif Bersamina <leifbersamina@gmail.com>
2021-08-07 01:29:39 +0800

    add generate_authentication_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
+
+  def generate_authentication_token
+    token = User.generate_unique_secure_token
+    update(authentication_token: token)
+  end
 end
